28bd5c6a89f71f3e342245d4c7da2e434a48bf5f
Samuel Williams <84680795+swilliamssw@users.noreply.github.com>
2024-04-19 17:43:56 +0100

    Script added to the intro-text

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // Wraps every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters');
+var textWrapper = document.querySelector('.intro-text .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
 anime.timeline({ loop: true })
     .add({
-        targets: '.ml11 .line',
+        targets: '.intro-text .line',
         scaleY: [0, 1],
         opacity: [0.5, 1],
         easing: "easeOutExpo",
         duration: 700
     })
     .add({
-        targets: '.ml11 .line',
-        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+        targets: '.intro-text .line',
+        translateX: [0, document.querySelector('.intro-text .letters').getBoundingClientRect().width + 10],
         easing: "easeOutExpo",
         duration: 700,
         delay: 100
     }).add({
-        targets: '.ml11 .letter',
+        targets: '.intro-text .letter',
         opacity: [0, 1],
         easing: "easeOutExpo",
         duration: 600,
         offset: '-=775',
         delay: (el, i) => 34 * (i + 1)
     }).add({
-        targets: '.ml11',
+        targets: '.intro-text',
         opacity: 0,
         duration: 1000,
         easing: "easeOutExpo",
